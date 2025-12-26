6f4627989f3ce9343b52c2241c228e4da143cdef
MatsX10 <tackskaduha@hotmail.com>
2025-12-27 00:46:26 +0100

    Minor fixes with user feedback

--- src/utils/accessibleErrorMsg.ts
+++ src/utils/accessibleErrorMsg.ts
@@ -5,12 +5,18 @@ export function getAccessibleAuthError(error: unknown): string {
     switch (error.code) {
       case "auth/email-already-in-use":
         return "There is already an existing account with this email";
+      case "auth/missing-password":
+        return "Please enter your password.";
+      case "auth/missing-email":
+        return "Please enter your email.";
       case "auth/invalid-email":
         return "The email is not valid.";
       case "auth/weak-password":
         return "The password has to contain at least 6 chars.";
       case "auth/user-not-found":
       case "auth/wrong-password":
+      case "auth/invalid-credential":
+      case "auth/invalid-login-credentials":
         return "Incorrect email or password";
       case "auth/too-many-requests":
         return "Too many attempts - please try again later";
